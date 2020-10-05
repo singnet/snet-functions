c315afa6d910225f2a36b045f3af64a85c672959
Vivek205 <ichbinvivek@gmail.com>
2020-10-05 13:57:13 +0530

    removed comments

--- appHeaders.js
+++ appHeaders.js
@@ -3,7 +3,6 @@
 const commonHeaders = require("./commonHeaders.json");
 
 module.exports.appHeaders = (event, _context, callback) => {
-    //Get contents of response
     const response = event.Records[0].cf.response;
     response.headers = {...commonHeaders, ...response.headers};
     callback(null, response);
