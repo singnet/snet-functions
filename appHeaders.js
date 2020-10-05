c03f9cc7f4f3df9a2b7a45dfdf29791496e6a999
Vivek205 <ichbinvivek@gmail.com>
2020-10-05 15:53:49 +0530

    added console statement

--- appHeaders.js
+++ appHeaders.js
@@ -3,7 +3,10 @@
 const commonHeaders = require("./commonHeaders.json");
 
 module.exports.appHeaders = (event, _context, callback) => {
+    console.log("inside appheaders")
     const response = event.Records[0].cf.response;
+    console.log("response", response)
     response.headers = {...commonHeaders, ...response.headers};
+    console.log("updated headers". response.headers)
     callback(null, response);
 };
